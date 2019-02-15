d74c11023cb67e3c3a3c14d1be1b881251d6e576
Randy Wilson <phoenixrr2113@gmail.com>
2019-02-15 11:51:05 -0500

    built actions REST API

--- api/server.js
+++ api/server.js
@@ -1,9 +1,14 @@
 const express = require('express'); //
 const morgan = require('morgan');
+const actionRouter = require('./routes/actionsRouter');
 
 const server = express();
 
-server.use(morgan);
+server.use(express.json());
+
+// server.use(morgan());
+
+server.use('/api/actions', actionRouter);
 
 server.get('/', async (req, res) => {
 	res.send(`
